7c63ab052b724d97ef88591573b8fd11e9ebacc8
Hugo Delahousse <hugo.delahousse@gmail.com>
2018-08-10 21:12:44 -0700

    Add rock destruction on collision

--- Assets/Scripts/RockScript.cs
+++ Assets/Scripts/RockScript.cs
@@ -11,7 +11,10 @@ public class RockScript : MonoBehaviour {
 	}
 	private state currentState = state.FIXED;
 
+	private float timePushed = 0;
+
 	public float pushSpeed;
+	public int projectileLayer;
 
 	Rigidbody2D rb2d;
 	// Use this for initialization
@@ -27,7 +30,23 @@ public class RockScript : MonoBehaviour {
 
 	public void getPushed(Vector2 direction) {
 		this.currentState = state.PUSHED;
+		Debug.LogError(projectileLayer);
+		gameObject.layer = projectileLayer;
 		rb2d.bodyType = RigidbodyType2D.Dynamic;
 		rb2d.velocity = pushSpeed * direction;
+		timePushed = Time.time;
+		BoxCollider2D collider = GetComponent<BoxCollider2D>();
+		collider.offset += Vector2.one * collider.size / 10;
+		collider.size = collider.size * 0.8f;
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (currentState != state.PUSHED) {
+			return;
+		}
+        Debug.LogError(gameObject.name + " " + other.gameObject);
+        // Get destroyed
+        Destroy(gameObject);
 	}
 }
