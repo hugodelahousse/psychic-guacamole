cc48a972a47b4c70a16734a9e5f1ead015cf9a30
Hugo Delahousse <hugo.delahousse@gmail.com>
2018-08-10 18:44:07 -0700

    Setup files

---
+++ Assets/Scripts/RockScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockScript : MonoBehaviour {
+
+	enum state {
+		FIXED,
+		HELD,
+		LAUNCHED,
+	}
+	private state currentState = state.FIXED;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
