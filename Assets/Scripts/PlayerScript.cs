8ba011de10a422ace9b0477dfdce55d4c941a35b
Hugo Delahousse <hugo.delahousse@gmail.com>
2018-08-10 22:22:35 -0700

    Add rock grabbing

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,58 +4,62 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-	enum state {
+    enum state {
         UNDECIDED,
-	}
+    }
 
-	private state currentState = state.UNDECIDED;
+    private state currentState = state.UNDECIDED;
     private Rigidbody2D rb2d;
     private bool grounded = false;
     private float lastJumpTime = 0;
 
-	private bool lastGrounded = false;
+    private bool lastGrounded = false;
 
-	[Header("Movement settings")]
-    public float speed;
+    private RockScript grabbedRock = null;
+
+    [Header("Movement settings")]
+        public float speed;
     public Vector2 maxSpeed;
-	public float jumpForce;
+    public float jumpForce;
 
-	private Animator anim;
+    private Animator anim;
 
     [Header("References")]
-    public LayerMask groundLayer;
+        public LayerMask groundLayer;
     public Transform groundChecker;
     public Transform rockFrontChecker;
+    public Transform rockBackChecker;
+    public Transform grabbedRocksPosition;
 
-	private bool stunned;
+    private bool stunned;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-	}
+    }
 
-	// Update is called once per frame
-	void FixedUpdate() {
+    // Update is called once per frame
+    void FixedUpdate() {
 
-		grounded = Physics2D.OverlapCircle(groundChecker.position, 0.25f, groundLayer);
-		if (lastGrounded == false && grounded == true) Debug.Log("landed"); // anim.SetTrigger("Landing");
-		lastGrounded = grounded;
+        grounded = Physics2D.OverlapCircle(groundChecker.position, 0.25f, groundLayer);
+        if (lastGrounded == false && grounded == true) Debug.Log("landed"); // anim.SetTrigger("Landing");
+        lastGrounded = grounded;
 
-		float moveHorizontal = Input.GetAxisRaw("Horizontal");
-		//anim.SetFloat("Velocity", moveHorizontal);
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        //anim.SetFloat("Velocity", moveHorizontal);
 
-		float moveVertical = 0;
+        float moveVertical = 0;
 
-		if (grounded && lastJumpTime + 0.1f < Time.time && Input.GetButtonDown("Jump"))
-		{
-			moveVertical = jumpForce;
-			//anim.SetTrigger("Jump");
-		}
+        if (grounded && lastJumpTime + 0.1f < Time.time && Input.GetButtonDown("Jump"))
+        {
+            moveVertical = jumpForce;
+            //anim.SetTrigger("Jump");
+        }
 
-		rb2d.AddForce(new Vector2(0, moveVertical) * speed);
-		rb2d.velocity = stunned ? new Vector2(rb2d.velocity.x, rb2d.velocity.y) : new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+        rb2d.AddForce(new Vector2(0, moveVertical) * speed);
+        rb2d.velocity = stunned ? new Vector2(rb2d.velocity.x, rb2d.velocity.y) : new Vector2(moveHorizontal * speed, rb2d.velocity.y);
 
-        if(grounded && lastJumpTime + 0.1f < Time.time  && Input.GetButtonDown("Jump"))
+        if(grounded && lastJumpTime + 0.1f < Time.time && Input.GetButtonDown("Jump"))
         {
             moveVertical = jumpForce;
             lastJumpTime = Time.time;
@@ -63,32 +67,53 @@ public class PlayerScript : MonoBehaviour {
 
         if (Input.GetButtonDown("Punch"))
         {
-            Punch();
+            bool punched = Punch();
+            if (!punched)
+                Grab();
         }
 
-		/*
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
-        rb2d.velocity = new Vector2(
-            Mathf.Min(maxSpeed.x, rb2d.velocity.x),
-            Mathf.Min(maxSpeed.y, rb2d.velocity.y)
-        );
-		*/
-	}
+        /*
+           Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+           rb2d.AddForce(movement * speed);
+           rb2d.velocity = new Vector2(
+           Mathf.Min(maxSpeed.x, rb2d.velocity.x),
+           Mathf.Min(maxSpeed.y, rb2d.velocity.y)
+           );
+           */
+    }
 
+    bool Grab() {
+        if (grabbedRock) return false;
+        Collider2D rockBack = Physics2D.OverlapCircle(rockBackChecker.position, 0.25f, groundLayer);
+        if (!rockBack) return false;
 
-    void Punch() {
-        Collider2D rockFront = Physics2D.OverlapCircle(rockFrontChecker.position, 0.25f, groundLayer);
-        if (rockFront)
+        RockScript rockScript = rockBack.GetComponent<RockScript>();
+        if (!rockScript) return false;
+        if (rockScript.getGrabbed(this))
+            grabbedRock = rockScript;
+
+        return true;
+    }
+
+    Vector2 getAimingDirection() {
+        return Vector2.right;
+    }
+
+    bool Punch() {
+        RockScript rockScript;
+        if (grabbedRock)
         {
-            RockScript rockScript = rockFront.GetComponent<RockScript>();
-            if (rockScript)
-            {
-                rockScript.getPushed(new Vector2(
-                    rockFront.transform.position.x > transform.position.x ? 1 : -1, 0
-                ));
-            }
+            rockScript = grabbedRock;
+            grabbedRock = null;
         }
+        else {
+            Collider2D rockFront = Physics2D.OverlapCircle(rockFrontChecker.position, 0.25f, groundLayer);
+            if (!rockFront) return false;
+            rockScript = rockFront.GetComponent<RockScript>();
+        }
+        if (!rockScript) return false;
+        rockScript.getPushed(getAimingDirection());
+        return true;
     }
 
 }
--- Assets/Scripts/RockScript.cs
+++ Assets/Scripts/RockScript.cs
@@ -14,18 +14,34 @@ public class RockScript : MonoBehaviour {
 	private float timePushed = 0;
 
 	public float pushSpeed;
+	public float grabbedSpeed;
 	public int projectileLayer;
 
-	Rigidbody2D rb2d;
+	private Rigidbody2D rb2d;
+
+	private PlayerScript owner;
+	private BoxCollider2D c2d;
 	// Use this for initialization
 
 	void Start () {
+		c2d = GetComponent<BoxCollider2D>();
 		rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		if (currentState == state.HELD) {
+			float distance = Vector2.Distance(transform.position, owner.grabbedRocksPosition.position);
+			if (distance > 2f) {
+				Vector2 direction = (owner.grabbedRocksPosition.position - transform.position).normalized;
+				rb2d.velocity = direction * grabbedSpeed * distance;
+			}
+		}
+	}
+
+	private void reduceColliderSize() {
+		c2d.offset += Vector2.one * c2d.size / 10;
+		c2d.size = c2d.size * 0.8f;
 	}
 
 	public void getPushed(Vector2 direction) {
@@ -34,15 +50,27 @@ public class RockScript : MonoBehaviour {
 		gameObject.layer = projectileLayer;
 		rb2d.bodyType = RigidbodyType2D.Dynamic;
 		rb2d.velocity = pushSpeed * direction;
+		c2d.isTrigger = false;
 		timePushed = Time.time;
-		BoxCollider2D collider = GetComponent<BoxCollider2D>();
-		collider.offset += Vector2.one * collider.size / 10;
-		collider.size = collider.size * 0.8f;
+	}
+
+	public bool getGrabbed(PlayerScript script) {
+		if( currentState != state.FIXED)
+            return false;
+		
+		currentState = state.HELD;
+		rb2d.bodyType = RigidbodyType2D.Dynamic;
+		reduceColliderSize();
+		owner = script;
+		Debug.LogError(script.grabbedRocksPosition.position);
+		c2d.isTrigger = true;
+
+		return true;
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
 	{
-		if (currentState != state.PUSHED) {
+		if (currentState != state.PUSHED || timePushed + 0.01f > Time.time) {
 			return;
 		}
         Debug.LogError(gameObject.name + " " + other.gameObject);
