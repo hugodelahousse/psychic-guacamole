489740329146fae63d0a81b1802185af11e59ae2
Andrew-Garcia <andrew.garcia.2015@gmail.com>
2018-08-10 23:38:44 -0500

    movement and particles

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,35 +15,51 @@ public class PlayerScript : MonoBehaviour {
     public float jumpForce;
 
     private bool grounded = false;
+	private bool lastGrounded = false;
     private float lastJumpTime = 0;
 
-
+	private Animator anim;
+	
     public LayerMask groundLayer;
     public Transform groundChecker;
+
+	private bool stunned;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate() {
 
-        grounded = Physics2D.OverlapCircle(groundChecker.position, 0.25f, groundLayer);
+		grounded = Physics2D.OverlapCircle(groundChecker.position, 0.25f, groundLayer);
+		if (lastGrounded == false && grounded == true) Debug.Log("landed"); // anim.SetTrigger("Landing");
+		lastGrounded = grounded;
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = 0;
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+		//anim.SetFloat("Velocity", moveHorizontal);
 
-        if(grounded && lastJumpTime + 0.1f < Time.time  && Input.GetButtonDown("Jump"))
-            moveVertical = jumpForce;
+		float moveVertical = 0;
 
+		if (grounded && lastJumpTime + 0.1f < Time.time && Input.GetButtonDown("Jump"))
+		{
+			moveVertical = jumpForce;
+			//anim.SetTrigger("Jump");
+		}
+
+		rb2d.AddForce(new Vector2(0, moveVertical) * speed);
+		rb2d.velocity = stunned ? new Vector2(rb2d.velocity.x, rb2d.velocity.y) : new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+
+
+		/*
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * speed);
         rb2d.velocity = new Vector2(
             Mathf.Min(maxSpeed.x, rb2d.velocity.x),
             Mathf.Min(maxSpeed.y, rb2d.velocity.y)
         );
-		
+		*/
 	}
 
 }
