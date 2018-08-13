2de3bf11cc0bf73cc4e3047ba0000b09c44936e9
Hugo Delahousse <hugo.delahousse@gmail.com>
2018-08-13 13:58:03 -0700

    Improve winner screen

---
+++ Assets/WinnerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinnerScript : MonoBehaviour {
+
+	[SerializeField] float updateTime;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("MoveText");
+	}
+
+	IEnumerator MoveText() {
+		RectTransform rectTransform = GetComponent<RectTransform>();
+		Vector2 size = rectTransform.rect.size;
+		while (true) {
+			yield return new WaitForSeconds(updateTime);
+			transform.position = new Vector2(
+				Random.Range(0, Screen.width - size.x),
+				Random.Range(0, Screen.height - size.y)
+			);
+		}
+	}
+}
